d13b1089543c2eb0b5cabe6bae6efaece449bc68
Zolmex <zolmexyt@gmail.com>
2025-10-07 13:44:35 -0300

    Draft back button

--- scripts/draft.js
+++ scripts/draft.js
@@ -1,6 +1,12 @@
 import { listenHome } from "./homeButton.js";
 
 function setup() {
+    const backBtn = document.getElementById('draft-panel-back-btn');
+
+    backBtn.addEventListener('click', () => {
+        window.location.href = "/index.html";
+    });
+
     listenHome(); // Home button listener
 }
 
